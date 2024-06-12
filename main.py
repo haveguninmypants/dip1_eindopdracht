a615776610ba0c2f7f52f0061d33780b096d907c
Wouter Weges <wg.weges@alfa-college.nl>
2024-06-12 10:57:18 +0200

    Eerste brakke versie

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-from flask import Flask, render_template
-from flask_login import login_required
+from flask import Flask, render_template, redirect, url_for
+from flask_login import login_required, LoginManager, UserMixin
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("./database.db")
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY, username TEXT, password INTEGER)''')
+    conn.commit()
+    conn.close()
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/login')
-def login():
+@login_required
+def inloggen():
     return render_template('login.html')
 
+@app.route('/register')
+@login_required
+def registreren():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return redirect(url_for('login'))
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return '''Hij doet het niet...
+            <p>
+            <a href="/">
+            <button>Home</button>
+            </a></p>'''
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    user = User()
+    user.id = username
+    return None
+
+
+@login_manager.request_loader
+def request_loader(request):
+    user = User()
+    return None
+
+create_db()
 
 if __name__ == '__main__':
     app.run(debug=True)
